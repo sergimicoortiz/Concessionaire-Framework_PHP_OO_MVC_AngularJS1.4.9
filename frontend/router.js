aaa17e2db6938f792e935dbf7cfc9e664991b732
Sergi Micó Ortiz <sergimicoortiz@gmail.com>
2022-05-09 17:07:23 +0200

    contact view

---
+++ frontend/router.js
@@ -0,0 +1,12 @@
+var app = angular.module('Concessionaire-Framework_PHP_OO_MVC_AngularJS1.4.9', ['ngRoute']);
+
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+        .when("/contact", {
+            templateUrl: "frontend/module/contact/view/contact.html"
+            //controller: "controller_home",
+        })//end contact
+        /* .otherwise("/contact", {
+            templateUrl: "frontend/module/contact/view/contact.html"
+        })//end default */
+}]);//end app.config
